f0534633f1ef72d035a489154b10980da4571a8c
James Chiang <jchiang87@gmail.com>
2026-01-25 14:49:32 -0800

    add LocalTaskVine class; refactor get_provider functions

--- bps_parsl_sites/task_vine.py
+++ bps_parsl_sites/task_vine.py
@@ -1,37 +1,54 @@
-from typing import TYPE_CHECKING, Any
-
 from parsl.executors.base import ParslExecutor
-from parsl.executors.taskvine import TaskVineExecutor, TaskVineManagerConfig
-from parsl.launchers import SrunLauncher
-from parsl.providers import SlurmProvider
+from parsl.executors.taskvine import (
+    TaskVineExecutor,
+    TaskVineManagerConfig,
+    TaskVineFactoryConfig,
+)
+from parsl.providers.base import ExecutionProvider
 
-try:
-    from parsl.providers.base import ExecutionProvider
-except ImportError:
-    from parsl.providers.provider_base import ExecutionProvider  # type: ignore
-
-from lsst.ctrl.bps.parsl.configuration import get_bps_config_value, \
-    get_workflow_name
+from lsst.ctrl.bps.parsl.configuration import get_bps_config_value
+from lsst.ctrl.bps.parsl.job import ParslJob
 from lsst.ctrl.bps.parsl.site import SiteConfig
 
-if TYPE_CHECKING:
-    from lsst.ctrl.bps.parsl.job import ParslJob
+from .utils import get_slurm_provider, get_local_provider
 
 
-__all__ = ["TaskVine"]
+__all__ = ["SlurmTaskVine", "LocalTaskVine"]
 
 
 class TaskVine(SiteConfig):
+    """Base class configuration for `TaskVineExecutor`.
+
+    Subclasses must provide implementations for ``.get_executors``
+    and ``.select_executor``.  In ``.get_executors``, the site-specific
+    `ExecutionProvider` must be defined.
+
+    Parameters
+    ----------
+    *args : `~typing.Any`
+        Parameters forwarded to base class constructor.
+    **kwargs : `~typing.Any`
+        Keyword arguments passed to base class constructor, augmented by
+        the ``resource_list`` argument.
+    """
+
     def __init__(self, *args, **kwargs):
         # Have BPS-defined resource requests for each job passed to task_vine.
-        kwargs["resource_list"] = ["memory", "cores", "disk",
-                                   "running_time_min", "priority"]
+        kwargs["resource_list"] = [
+            "memory",
+            "cores",
+            "disk",
+            "running_time_min",
+            "priority",
+        ]
         super().__init__(*args, **kwargs)
 
     def make_executor(
-            self,
-            label: str,
-            provider: ExecutionProvider,
+        self,
+        label: str,
+        provider: ExecutionProvider,
+        worker_options: str | None = None,
+        tv_max_retries: int = 1,
     ) -> ParslExecutor:
         """Return a `TaskVineExecutor`.
 
@@ -41,17 +58,37 @@ class TaskVine(SiteConfig):
             Label for executor.
         provider : `ExecutionProvider`
             Parsl execution provider, e.g., `SlurmProvider`.
+        worker_options : `str`, optional
+            Extra options to pass to work_queue workers, e.g.,
+            ``"--memory=90000"``. Default: `None`.
+        tv_max_retries : `int`, optional
+            Number of retries for task_vine to attempt per job.  Set to
+            ``None`` to have it try indefinitely; set to ``1`` to have Parsl
+            control the number of retries.  Default: ``1``.
         """
-        manager_config = TaskVineManagerConfig(address=self.get_address())
+        worker_options = get_bps_config_value(
+            self.site, "worker_options", str, worker_options
+        )
+        max_retries = get_bps_config_value(
+            self.site, "tv_max_retries", int, tv_max_retries
+        )
+        manager_config = TaskVineManagerConfig(
+            address=self.get_address(), max_retries=max_retries
+        )
+        factory_config = TaskVineFactoryConfig(worker_options=worker_options)
         return TaskVineExecutor(
             label=label,
             worker_launch_method="provider",
             manager_config=manager_config,
+            factory_config=factory_config,
             provider=provider,
         )
 
+
+class SlurmTaskVine(TaskVine):
+
     def get_executors(self) -> list[ParslExecutor]:
-        return [self.make_executor("task_vine", self.get_provider())]
+        return [self.make_executor("slurm_task_vine", get_slurm_provider(self))]
 
     def select_executor(self, job: "ParslJob") -> str:
         """Get the ``label`` of the executor to use to execute a job
@@ -66,69 +103,25 @@ class TaskVine(SiteConfig):
         label : `str`
             Label of executor to use to execute ``job``.
         """
-        return "task_vine"
+        return "slurm_task_vine"
 
-    def get_provider(
-            self,
-            nodes: int | None = 1,
-            cores_per_node: int | None = None,
-            walltime: str | None = None,
-            mem_per_node: int | None = None,
-            qos: str | None = None,
-            constraint: str | None = None,
-            singleton: bool = False,
-            exclusive: bool = False,
-            scheduler_options: str | None = None,
-            provider_options: dict[str, Any] | None = None,
-    ) -> ExecutionProvider:
-        """Return a SlurmProvider."""
-        nodes = get_bps_config_value(self.site, "nodes_per_block", int, 1)
-        cores_per_node = get_bps_config_value(self.site, "cores_per_node",
-                                              int, cores_per_node)
-        walltime = get_bps_config_value(self.site, "walltime", str, walltime,
-                                        required=True)
-        mem_per_node = get_bps_config_value(self.site, "mem_per_node", int,
-                                            mem_per_node)
-        qos = get_bps_config_value(self.site, "qos", str, qos)
-        constraint = get_bps_config_value(self.site, "constraint", str,
-                                          constraint)
-        singleton = get_bps_config_value(self.site, "singleton", bool,
-                                         singleton)
-        exclusive = get_bps_config_value(self.site, "exclusive", bool,
-                                         exclusive)
-        scheduler_options = get_bps_config_value(
-            self.site, "scheduler_options", str, scheduler_options)
 
-        # Replace any filepath separators with underscores since Parsl
-        # creates a shell script named f"cmd_{job_name}.sh" using the
-        # --job-name value in the sbatch script.
-        job_name = get_workflow_name(self.config).replace("/", "_")
-        if scheduler_options is None:
-            scheduler_options = ""
-        scheduler_options += "\n"
-        scheduler_options += f"#SBATCH --job-name={job_name}\n"
-        if qos:
-            scheduler_options += f"#SBATCH --qos={qos}\n"
-        if constraint:
-            scheduler_options += f"#SBATCH --constraint={constraint}\n"
-        if singleton:
-            # The following SBATCH directives allow only a single
-            # slurm job (parsl block) with our job_name to run at
-            # once. This means we can have one job running, and one
-            # already in the queue when the first exceeds the walltime
-            # limit. More backups could be achieved with a larger
-            # value of max_blocks.  This only allows one job to be
-            # actively running at once, so that needs to be sized
-            # appropriately by the user.
-            scheduler_options += "#SBATCH --dependency=singleton\n"
-        provider = SlurmProvider(
-            nodes_per_block=nodes,
-            cores_per_node=cores_per_node,
-            mem_per_node=mem_per_node,
-            walltime=walltime,
-            exclusive=exclusive,
-            scheduler_options=scheduler_options,
-            launcher=SrunLauncher(overrides="-K0 -k --cpu-bind=none"),
-            **(provider_options or {}),
-        )
-        return provider
+class LocalTaskVine(TaskVine):
+
+    def get_executors(self) -> list[ParslExecutor]:
+        return [self.make_executor("local_task_vine", get_local_provider(self))]
+
+    def select_executor(self, job: "ParslJob") -> str:
+        """Get the ``label`` of the executor to use to execute a job
+
+        Parameters
+        ----------
+        job : `ParslJob`
+            Job to be executed.
+
+        Returns
+        -------
+        label : `str`
+            Label of executor to use to execute ``job``.
+        """
+        return "local_task_vine"
